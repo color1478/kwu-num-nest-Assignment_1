41a1a0d40f2817eb6dd6eb2cb8fc7ca26140ee9b
color1478 <jehohi04@naver.com>
2024-11-04 00:50:14 +0900

    assignment_1_final

--- lib.js
+++ lib.js
@@ -1,24 +1,43 @@
-function avg(numbers) {
+function avg(numbers)
+{
     let s = 0;
-    for (let i = 0; i < numbers.length; i++) {
+    for (let i = 0; i < numbers.length; i++)
+        {
         s += numbers[i];
     }
     return s / numbers.length;
 }
 
-function prime(num) {
-    for (let i = 2; i < num; i++) {
-        if (num % i === 0) return false;
+function prime(num)
+{
+    if (num <= 1)
+        {
+            return false;
+        }
+    for (let i = 2; i <= Math.sqrt(num); i++)
+    {
+        if (num % i === 0)
+            {
+                return false;
+            }
     }
     return true;
 }
 
-function factorial(num) {
-    let res = 1;
-    for (let i = num; i >= 2; i--) {
-        res *= i;
+function factorial(num)
+{
+    if (num < 0 || num > 15)
+    {
+        return("범위 오류 (0~15)");
     }
-    return res;
+    else{
+        let res = 1;
+        for (let i = num; i >= 2; i--)
+        {
+            res *= i;
+        }
+        return res;
+    }
 }
 
 module.exports = {
--- lib.test.js
+++ lib.test.js
@@ -1,10 +1,36 @@
 const { test, expect } = require("@jest/globals");
 const lib = require("./lib");
 
+// avg 함수 테스트
 test("avg([3, 5, 7]) should be 5", () => {
     expect(lib.avg([3, 5, 7])).toBe(5);
 });
 
 test("avg([-5, 5]) should be 0", () => {
     expect(lib.avg([-5, 5])).toBe(0);
+});
+
+// prime 함수 테스트
+//소수일 때
+test("prime(7) should return true", () => {
+    expect(lib.prime(7)).toBe(true);
+});
+//소수가 아닐 때
+test("prime(4) should return false", () => {
+    expect(lib.prime(4)).toBe(false);
+});
+
+// factorial 함수 테스트
+//일반적인 경우
+test("factorial(5) should return 120", () => {
+    expect(lib.factorial(5)).toBe(120);
+});
+//0인 경우
+test("factorial(0) should return 1", () => {
+    expect(lib.factorial(0)).toBe(1);
+});
+
+//범위 초과 경우
+test("factorial(16) should return 범위 오류 (0~15)", () => {
+    expect(lib.factorial(16)).toBe("범위 오류 (0~15)");
 });
